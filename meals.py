5191273ae3edd62c1e87b9bb96259e022623415c
Madelyn Gueta <madelyngueta306@gmail.com>
2026-05-22 14:41:20 +0800

    Delete meals.py

--- meals.py
+++
@@ -1,8 +0,0 @@
-class Meal:
-
-    def __init__(self, date, meal_type, food, calories):
-
-        self.date = date
-        self.meal_type = meal_type
-        self.food = food
-        self.calories = calories
