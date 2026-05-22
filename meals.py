218d6c160e71e4d351cb01cbaa7a0580e050953d
Madelyn Gueta <madelyngueta306@gmail.com>
2026-05-22 14:39:53 +0800

    Create meals.py

---
+++ meals.py
@@ -0,0 +1,8 @@
+class Meal:
+
+    def __init__(self, date, meal_type, food, calories):
+
+        self.date = date
+        self.meal_type = meal_type
+        self.food = food
+        self.calories = calories
